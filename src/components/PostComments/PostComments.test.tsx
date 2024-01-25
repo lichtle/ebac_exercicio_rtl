828fa32394fc2fd1f31e4cf5448eac5b49f4fbc8
“lichtle” <leticia.negreiros@outlook.com>
2024-01-25 19:24:02 -0300

    chore: tarefa finalizada

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,22 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  it("Deve inserir dois comentários", () => {
+    const { debug } = render(<PostComment />);
+    const input = screen.getByTestId("input");
+
+    fireEvent.change(input, { target: { value: input.value.length > 0 } });
+    fireEvent.click(screen.getByTestId("btn-adicionar-comentario"));
+    fireEvent.change(input, { target: { value: input.value.length > 0 } });
+    fireEvent.click(screen.getByTestId("btn-adicionar-comentario"));
+    debug();
+    expect(screen.getByTestId("comentarios").children).toHaveLength(2);
+  });
 });
